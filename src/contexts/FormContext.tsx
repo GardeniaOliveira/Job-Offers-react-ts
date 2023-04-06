c5f81d30c18ce670291bcc8f4fde2a6da9678424
Gardenia Oliveira <jobs.gardeniaoliveira@gmail.com>
2023-04-06 16:14:36 +0100

    created context and set types

--- src/contexts/FormContext.tsx
+++ src/contexts/FormContext.tsx
@@ -1,20 +1,43 @@
 //Context, Reducer, Provider, Hook
 
-import { createContext, useContext, useReducer } from "react";
+import { createContext, useContext, useReducer, ReactNode } from "react";
+import { State } from "../types/State";
+// import { Action } from "../types/Action";
+
+type Action = {
+  type: FormAction;
+  payload: any;
+};
+
+type ContextType = {
+  state: State;
+  dispatch: (action: Action) => void;
+};
+
+const initialData: State = {
+  currentStep: 0,
+  name: "",
+  level: 0,
+  email: "",
+  github: "",
+};
+
+type FormProviderProps = {
+  children: ReactNode;
+};
 
 //Context
-
-const Context = createContext(undefined);
+const FormContext = createContext<ContextType | undefined>(undefined);
 
 //reducer
-enum FormAction {
+export enum FormAction {
   setCurrentStep,
   setName,
   setLevel,
   setEmail,
   setGithub,
 }
-const FormReducer = (state, action) => {
+const FormReducer = (state: State, action: Action) => {
   switch (action.type) {
     case FormAction.setCurrentStep:
       //pass the initial state (...state) to the action and return the state after the action
@@ -35,4 +58,20 @@ const FormReducer = (state, action) => {
     default:
       return state;
   }
+};
+
+//Provider
+export const FormProvider = ({ children }: FormProviderProps) => {
+  const [state, dispatch] = useReducer(FormReducer, initialData);
+  const value = { state, dispatch };
+  return <FormContext.Provider value={value}>{children}</FormContext.Provider>;
+};
+
+//context Hook
+export const useForm = () => {
+  const context = useContext(FormContext);
+  if (context === undefined) {
+    throw new Error("useForm needs to be used into a FormProvider");
+  }
+  return context;
 };
