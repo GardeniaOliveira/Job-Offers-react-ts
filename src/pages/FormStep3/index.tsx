99d91a6e9377282899218dc305ff25f267938948
Gardenia Oliveira <jobs.gardeniaoliveira@gmail.com>
2023-04-06 21:14:24 +0100

    created step3

--- src/pages/FormStep3/index.tsx
+++ src/pages/FormStep3/index.tsx
@@ -1,5 +1,81 @@
+import { ChangeEvent, useEffect } from "react";
+import { useNavigate, Link } from "react-router-dom";
 import * as C from "./styles";
+import { Theme } from "../../components/Theme";
+import { useForm, FormActions } from "../../contexts/FormContext";
 
 export const FormStep3 = () => {
-  return <C.Container>Step 3</C.Container>;
+  const navigate = useNavigate();
+  //state has the data and dispatch changes the data
+  const { state, dispatch } = useForm();
+
+  useEffect(() => {
+    if (state.name === "") {
+      navigate("/");
+    } else {
+      dispatch({
+        type: FormActions.setCurrentStep,
+        payload: 3,
+      });
+    }
+  }, []);
+
+  const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
+    dispatch({
+      type: FormActions.setEmail,
+      payload: e.target.value,
+    });
+  };
+
+  const handleGithubChange = (e: ChangeEvent<HTMLInputElement>) => {
+    dispatch({
+      type: FormActions.setGithub,
+      payload: e.target.value,
+    });
+  };
+
+  const handleNextStep = () => {
+    if (state.email !== "" && state.github !== "") {
+      console.log(state);
+    } else {
+      alert("Please, type the fields below");
+    }
+  };
+
+  return (
+    <Theme>
+      <C.Container>
+        <p>Step 3/3 </p>
+        <h1>Nice, {state.name}. Where can we find you?</h1>
+
+        <p>Fill in the fields below with your contacts </p>
+        <hr />
+        <label>
+          Your email:
+          <input
+            type="email"
+            value={state.email}
+            onChange={handleEmailChange}
+          />
+        </label>
+
+        <label>
+          Your GitHub:
+          <input
+            type="url"
+            value={state.github}
+            onChange={handleGithubChange}
+          />
+        </label>
+
+        <button>
+          {" "}
+          <Link to="/step2" className="backBtn">
+            Previous
+          </Link>{" "}
+        </button>
+        <button onClick={handleNextStep}>Finish registration</button>
+      </C.Container>
+    </Theme>
+  );
 };
--- src/pages/FormStep3/styles.ts
+++ src/pages/FormStep3/styles.ts
@@ -1,3 +1,56 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  h1 {
+    font-size: 26px;
+    margin: 0;
+    padding: 0;
+  }
+  p {
+    font-size: 14px;
+    color: #b8b8d4;
+  }
+
+  hr {
+    height: 1px;
+    border: 0;
+    background-color: #16195c;
+    margin: 30px 0;
+  }
+
+  label {
+    font-size: 14px;
+    display: block;
+    margin-bottom: 20px;
+
+    input {
+      display: block;
+      box-sizing: border-box;
+      width: 100%;
+      margin-top: 7px;
+      padding: 8px;
+      border: 1px solid transparent;
+      border-radius: 30px;
+      outline: 0;
+      font-size: 16px;
+      color: #020617;
+    }
+  }
+
+  button {
+    background-color: #25cd89;
+    color: #020617;
+    font-size: 14px;
+    font-weight: bold;
+    padding: 10px 40px;
+    border: 0;
+    border-radius: 30px;
+    margin: 30px 30px 30px 0;
+    cursor: pointer;
+
+    .backBtn {
+      text-decoration: none;
+      color: rgb(22, 25, 92);
+    }
+  }
+`;
