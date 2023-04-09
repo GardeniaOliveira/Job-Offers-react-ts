42e75054752c6bbdd6a4aca72bcfd329dd69f706
Gardenia Oliveira <jobs.gardeniaoliveira@gmail.com>
2023-04-09 23:35:36 +0100

    update readme

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,7 +3,7 @@ import * as C from "./styles";
 export const Footer = () => {
   return (
     <C.Footer>
-      Project based on Bonieky Lacerda's tutorial - update by Gardênia Oliveira
+      Project based on Bonieky Lacerda's tutorial - upgrade by Gardênia Oliveira
       - Data from The JSearch API <br />
     </C.Footer>
   );
